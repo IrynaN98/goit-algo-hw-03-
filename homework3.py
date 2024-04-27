e1de4bab591178169983458518db06238a2d4fe1
IrynaN98 <163687352+IrynaN98@users.noreply.github.com>
2024-04-27 10:20:30 +0200

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,27 @@
+from random import randint, sample
+min = 1 
+max = 36
+quantity = 5
+
+
+
+def get_numbers_ticket(min, max, quantity):
+    if not (1 <= min <= max <= 1000) or not (1. <= quantity <= (max - min +1)):
+        return []
+     
+    result_array = set()
+    while len(result_array) != quantity:
+        result_array.add(randint(min, max))
+    
+    return sorted (result_array)
+       
+
+
+print(sorted(sample(range(min, max), quantity)))
+    
+
+
+    
+
+
+
